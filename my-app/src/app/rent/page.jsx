978a70f96a1da4106f684b9774885b73cfef5837
mmdmg21 <sinnerfathermg@gmail.com>
2024-08-06 08:37:01 +0330

    Add files via upload

---
+++ my-app/src/app/rent/page.jsx
@@ -0,0 +1,26 @@
+import ListingItem from '@/components/ListingItem';
+import { defineOneEntry } from 'oneentry';
+
+const { Products } = defineOneEntry('https://mmddatabase.oneentry.cloud', {
+  token: process.env.NEXT_PUBLIC_ONEENTRY_TOKEN,
+});
+
+export default async function Rent() {
+  const rentListings = await Products.getProductsByPageUrl('rent');
+  return (
+    <div>
+      <h1 className='text-3xl font-semibold text-slate-600 text-center my-10'>
+        Recent places for rent
+      </h1>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+        {rentListings && rentListings.length > 0 && (
+          <div className='flex flex-wrap gap-4'>
+            {rentListings.map((listing) => (
+              <ListingItem listing={listing} key={listing.id} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ my-app/src/app/sell/page.jsx
@@ -0,0 +1,26 @@
+import ListingItem from '@/components/ListingItem';
+import { defineOneEntry } from 'oneentry';
+
+const { Products } = defineOneEntry('https://mmdatabse.oneentry.cloud', {
+  token: process.env.NEXT_PUBLIC_ONEENTRY_TOKEN,
+});
+
+export default async function Sell() {
+  const sellListings = await Products.getProductsByPageUrl('sell');
+  return (
+    <div>
+      <h1 className='text-3xl font-semibold text-slate-600 text-center my-10'>
+        Recent places for sell
+      </h1>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+        {sellListings && sellListings.length > 0 && (
+          <div className='flex flex-wrap gap-4'>
+            {sellListings.map((listing) => (
+              <ListingItem listing={listing} key={listing.id} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
